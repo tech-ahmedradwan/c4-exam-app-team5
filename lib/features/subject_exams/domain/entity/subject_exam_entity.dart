8d00c4962281ca554eba3b6e340a92204ed36700
Ahmed Radwan <tech.ahmedradwan@gmail.com>
2025-11-16 13:44:42 +0200

    feat: add SubjectExamEntity, SubjectExamRepoContract, and GetSubjectExamUsecase for subject exam management

---
+++ lib/features/subject_exams/domain/entity/subject_exam_entity.dart
@@ -0,0 +1,12 @@
+class SubjectExamEntity {
+  const SubjectExamEntity({
+    this.id,
+    this.title,
+    this.duration,
+    this.numberOfQuestions,
+  });
+  final String? id;
+  final String? title;
+  final int? duration;
+  final int? numberOfQuestions;
+}
